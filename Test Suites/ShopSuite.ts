9772b3430db27c2ea715ad4735147d7b6814815a
MarwaYoussef1 <marwa.abdelrahmaan@gmail.com>
2024-07-18 15:27:00 +0200

    [18.7.2024]

---
+++ Test Suites/ShopSuite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ShopSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a60882e8-e0ba-48f3-a4b0-b3a0a35d41a8</testSuiteGuid>
+</TestSuiteEntity>
